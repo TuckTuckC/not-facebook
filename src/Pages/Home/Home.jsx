1f519356877158554966f6da9e12a4046c610673
TuckTuckC <mrkittay847@gmail.com>
2021-06-06 22:40:29 -0400

    added google login

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {GoogleLogin, GoogleLogout, useGoogleLogin, useGoogleLogout} from 'react-google-login';
+
 const Home = () => {
+
+    const responseGoogle = (response) => {
+        console.log(response);
+    }
+
     return (
         <div>
+            <GoogleLogin clientId="657238987331-ejs8f81k6le8n4rs2tk7ch0gk69ht4vl.apps.googleusercontent.com" 
+            buttonText="Login with Google" 
+            onSuccess={responseGoogle} 
+            onFailure={responseGoogle}
+            cookiePolicy="single_host_origin" />
+            {/* <GoogleLogout
+            clientId="657238987331-ejs8f81k6le8n4rs2tk7ch0gk69ht4vl.apps.googleusercontent.com"
+            buttonText="Logout">
+            </GoogleLogout> */}
             Yo
         </div>
     );
