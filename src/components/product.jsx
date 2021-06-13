dbc8e25c7026a0b759e466a06d5a44e80807805e
TuckTuckC <mrkittay847@gmail.com>
2021-06-13 11:34:42 -0400

    added more information on the product

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,8 +1,10 @@
 const product = () => {
-
+    
+    const type = 'sub';
     let name = 'CompC';
     let brand = 'Kicker';
     let price = 89.99;
+    let size = '12"'
     let rms = 300;
     let peak = 600;
     let description = 'Enjoy every bit of bass from your car stereo with this Stillwater Designs subwoofer. The 12-inch CompC voice coil subwoofer is compatible with a range of setups and offers 4 ohms impedance for reliable sound. The ribbed polyurethane surround on this Stillwater Designs subwoofer eliminates unwanted vibrations, and the classic yellow stitching adds a bit of style.'
@@ -12,6 +14,7 @@ const product = () => {
         <div>
             <h1>{brand}{name}</h1>
             <p>${price}</p>
+            <p>Size: {size}</p>
             <p>RMS: {rms}</p>
             <p>Peak Wattage: {peak}</p>
             <p>Description: {description}</p>
